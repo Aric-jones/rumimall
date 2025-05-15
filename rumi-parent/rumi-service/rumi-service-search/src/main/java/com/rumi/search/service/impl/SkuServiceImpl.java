e011eaffe0abdc65e92c6586c5e4a46a220d6b2b
CSH <2926480799@qq.com>
2025-05-15 23:02:23 +0800

    feat:商品搜索:品牌统计，规格统计，条件筛选，分页，排序

--- rumi-parent/rumi-service/rumi-service-search/src/main/java/com/rumi/search/service/impl/SkuServiceImpl.java
+++ rumi-parent/rumi-service/rumi-service-search/src/main/java/com/rumi/search/service/impl/SkuServiceImpl.java
@@ -1,6 +1,7 @@
 package com.rumi.search.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson.JSON;
 import com.rumi.common.entity.Result;
 import com.rumi.goods.feign.SkuFeign;
@@ -8,10 +9,14 @@ import com.rumi.goods.pojo.Sku;
 import com.rumi.search.dao.SkuEsMapper;
 import com.rumi.search.pojo.SkuInfo;
 import com.rumi.search.service.ISkuService;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
@@ -66,52 +71,164 @@ public class SkuServiceImpl implements ISkuService {
     @Override
     public Map<String, Object> search(Map<String, Object> searchMap) {
 
-        // 使用关键词进行搜索
-        String keywords = (String) searchMap.get("keywords");
+        // 构建查询条件并查询
+        AggregatedPage<SkuInfo> page = buildQueryAndSearch(searchMap);
 
-        if (StringUtils.isEmpty(keywords)) {
-            keywords = "华为";
+        // 获取聚合结果
+        List<String> categoryList = makeTheList(page,"skuCategory");
+        List<String> brandList = makeTheList(page,"skuBrand");
+        Map<String, Set<String>> specMap = makeTheListForSpec(page, "skuSpec");
+
+        // 构建返回结果
+        Map<String, Object> resultMap = searchList(page);
+        if(Objects.isNull(searchMap) || Objects.isNull(searchMap.get("category"))){
+            resultMap.put("categoryList",categoryList);
+        }
+        if(Objects.isNull(searchMap) || Objects.isNull(searchMap.get("brand"))){
+            resultMap.put("brandList",brandList);
+        }
+        resultMap.put("specMap",specMap);
+        return resultMap;
+    }
+
+    private  Map<String, Set<String>> makeTheListForSpec(AggregatedPage<SkuInfo> page, String skuSpec) {
+        // 处理聚合结果
+        StringTerms stringTermsCategory = (StringTerms) page.getAggregation(skuSpec);
+        List<String> list = new ArrayList<>();
+        if (stringTermsCategory != null) {
+            for (StringTerms.Bucket bucket : stringTermsCategory.getBuckets()) {
+                String keyAsString = bucket.getKeyAsString();
+                System.out.println(keyAsString);
+                list.add(keyAsString);
+            }
+        }
+        Map<String, Set<String>> allSpec = new HashMap<>();
+        // 将spec转成对象
+        for(String spec : list){
+            Map<String,String> specMap = JSONUtil.toBean(spec, Map.class);
+            for(String key : specMap.keySet()){
+                String value = specMap.get(key);
+                Set<String> set = allSpec.get(key);
+                if(set == null){
+                    set = new HashSet<>();
+                }
+                set.add(value);
+                allSpec.put(key,set);
+            }
+        }
+        return allSpec;
+    }
+
+    private static List<String> makeTheList(AggregatedPage<SkuInfo> page,String field) {
+
+        // 处理聚合结果
+        StringTerms stringTermsCategory = (StringTerms) page.getAggregation(field);
+        List<String> list = new ArrayList<>();
+        if (stringTermsCategory != null) {
+            for (StringTerms.Bucket bucket : stringTermsCategory.getBuckets()) {
+                String keyAsString = bucket.getKeyAsString();
+                System.out.println(keyAsString);
+                list.add(keyAsString);
+            }
+        }
+        return list;
+    }
+
+    private static Map<String, Object> searchList(AggregatedPage<SkuInfo> page) {
+        // 构建返回结果
+        List<SkuInfo> content = page.getContent();
+        int totalPages = page.getTotalPages();
+        long totalElements = page.getTotalElements();
+        Map<String,Object> resultMap =new HashMap<>();
+        resultMap.put("rows",content);
+        resultMap.put("total",totalElements);
+        resultMap.put("totalPages",totalPages);
+        return resultMap;
+    }
+
+    private AggregatedPage<SkuInfo> buildQueryAndSearch(Map<String, Object> searchMap) {
+        NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
+        BoolQueryBuilder boolQueryBuilder = new BoolQueryBuilder();
+        if(searchMap !=null&& !searchMap.isEmpty()){
+            // 使用关键词进行搜索
+            String keywords = (String) searchMap.get("keywords");
+            // 如果关键词不为空
+            if(!StringUtils.isEmpty(keywords)){
+                /**
+                 * 设置主查询条件
+                 * withQuery()：设置主查询条件，需要传入一个查询条件
+                 * QueryBuilders.matchQuery("name",keywords)：构建一个 match 查询，用于在 name 字段中搜索包含 keywords 的文档
+                 */
+                // nativeSearchQueryBuilder.withQuery(QueryBuilders.matchQuery("name",keywords));
+                boolQueryBuilder.must(QueryBuilders.queryStringQuery(keywords).field("name"));
+            }
+            if(Objects.nonNull(searchMap.get("category"))){
+                boolQueryBuilder.must(QueryBuilders.termQuery("categoryName",searchMap.get("category")));
+            }
+            if(Objects.nonNull(searchMap.get("brand"))){
+                boolQueryBuilder.must(QueryBuilders.termQuery("brandName",searchMap.get("brand")));
+            }
+
+            for(String key : searchMap.keySet()){
+                if(key.startsWith("spec_")){
+                    boolQueryBuilder.must(QueryBuilders.termQuery("specMap."+key.substring(5)+".keyword",searchMap.get(key)));
+                }
+            }
+
+            //价格过滤查询
+            String price = (String)searchMap.get("price");
+            if (!StringUtils.isEmpty(price)) {
+                price = price.replace("元","").replace("以上", "");
+                String[] split = price.split("-");
+                if(!price.isEmpty()){
+                    boolQueryBuilder.must(QueryBuilders.rangeQuery("price").gt(Integer.parseInt(split[0])));
+                }
+                if(split.length>1){
+                    boolQueryBuilder.must(QueryBuilders.rangeQuery("price").lte(Integer.parseInt(split[1])));
+                }
+            }
+
+            //构建排序查询
+            String sortRule = (String) searchMap.get("sortRule");
+            String sortField = (String) searchMap.get("sortField");
+            if (!StringUtils.isEmpty(sortRule) && !StringUtils.isEmpty(sortField)) {
+                nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort(sortField).order(sortRule.equals("DESC") ? SortOrder.DESC : SortOrder.ASC));
+            }
         }
 
-        // 构建搜索条件
-        NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
+
+
         /**
          * 构建聚合查询获取分类列表
          * terms("skuCategory"): 聚合的名称，相当于别名
          * field("categoryName"): 要聚合字段
          * size(50): 聚合结果集的大小
          */
-        nativeSearchQueryBuilder.addAggregation(AggregationBuilders.terms("skuCategory").field("categoryName").size(50));
-        /**
-         * 设置主查询条件
-         * withQuery()：设置主查询条件，需要传入一个查询条件
-         * QueryBuilders.matchQuery("name",keywords)：构建一个 match 查询，用于在 name 字段中搜索包含 keywords 的文档
-         */
-        nativeSearchQueryBuilder.withQuery(QueryBuilders.matchQuery("name",keywords));
-        // 执行查询
-        NativeSearchQuery nativeSearchQuery = nativeSearchQueryBuilder.build();
-        AggregatedPage<SkuInfo> skuInfos = elasticsearchTemplate.queryForPage(nativeSearchQuery, SkuInfo.class);
+        if(Objects.isNull(searchMap) || Objects.isNull(searchMap.get("category"))){
+            nativeSearchQueryBuilder.addAggregation(AggregationBuilders.terms("skuCategory").field("categoryName").size(2000));
+        }
+        if(Objects.isNull(searchMap) || Objects.isNull(searchMap.get("brand"))){
+            nativeSearchQueryBuilder.addAggregation(AggregationBuilders.terms("skuBrand").field("brandName").size(2000));
+        }
+        nativeSearchQueryBuilder.addAggregation(AggregationBuilders.terms("skuSpec").field("spec.keyword").size(2000));
 
-        // 处理聚合结果
-        StringTerms stringTermsCategory = (StringTerms) skuInfos.getAggregation("skuCategory");
-        List<String> categoryList = new ArrayList<>();
-        if (stringTermsCategory != null) {
-            for (StringTerms.Bucket bucket : stringTermsCategory.getBuckets()) {
-                String keyAsString = bucket.getKeyAsString();
-                System.out.println(keyAsString);
-                categoryList.add(keyAsString);
+        // 分页查询
+        Integer pageNum = coverterPage(searchMap);
+        Integer size = 3;
+        nativeSearchQueryBuilder.withPageable(PageRequest.of(pageNum-1, size));
+        nativeSearchQueryBuilder.withQuery(boolQueryBuilder);
+
+        return elasticsearchTemplate.queryForPage(nativeSearchQueryBuilder.build(), SkuInfo.class);
+    }
+
+    private Integer coverterPage(Map<String, Object> searchMap) {
+        if(searchMap!=null){
+            String pageNum = (String) searchMap.get("pageNum");
+            try {
+                return Integer.parseInt(pageNum);
+            }catch (NumberFormatException e){
             }
         }
-
-        // 构建返回结果
-        List<SkuInfo> content = skuInfos.getContent();
-        int totalPages = skuInfos.getTotalPages();
-        long totalElements = skuInfos.getTotalElements();
-        Map<String,Object> resultMap =new HashMap<>();
-        resultMap.put("categoryList",categoryList);
-        resultMap.put("rows",content);
-        resultMap.put("total",totalElements);
-        resultMap.put("totalPages",totalPages);
-        return resultMap;
+        return 1;
     }
 }
